664255026b69231b801b3c66beb65d56e9af3eb5
Julien Lhermitte <jrmlhermitte@gmail.com>
2017-10-06 11:47:06 -0400

    PEP8 : utils

--- SciStreams/utils/mask.py
+++ SciStreams/utils/mask.py
@@ -1,10 +1,11 @@
 import numpy as np
 # mask utilities
 
+
 def load_master_mask(filename):
     ''' load a master mask format '''
     res = np.load(filename)
-    mask =  res['mask']
-    refpoint =  res['refpoint']
+    mask = res['mask']
+    refpoint = res['refpoint']
     rotation_center = res['rotation_center']
     return mask, refpoint, rotation_center
