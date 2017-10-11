adee84abec909e5770b059853a52e4a9c2da0fc0
Julien Lhermitte <jrmlhermitte@gmail.com>
2017-10-11 10:50:28 -0400

    fixed object hash test

--- SciStreams/tests/test_stream_objectpass.py
+++ SciStreams/tests/test_stream_objectpass.py
@@ -2,11 +2,13 @@
 from dask import delayed, compute, get, set_options
 # for testing the caching
 from dask.base import normalize_token
+from dask.cache import Cache
 
-from streamz import Stream
-import SciStreams.core.scistreams as scs
-from SciStreams.core.StreamDoc import StreamDoc
 
+# set up the cache
+cache_tmp = Cache(1e9)
+cache_tmp.register()
+# make everything pure by default
 set_options(delayed_pure=True)
 
 
@@ -33,23 +35,8 @@ def test_object_hash():
     second = delayed(add)(myobj2)
     assert first.key == second.key
 
-    # next, check it also works in simple streams (use scistreams)
-    s = Stream()
-    # when delayed, should cache, the second map is in streams not SciStreams
-    sout = scs.map(delayed(add), s)
-    sout = scs.map(lambda x: compute(x['args'][0], get=get), sout)
-    lout = sout.sink_to_list()
-
-    s2 = Stream()
-    s2out = scs.map(add, s2)
-    l2out = s2out.sink_to_list()
-
-    s.emit(StreamDoc(args=(myobj)))
-    s.emit(StreamDoc(args=(myobj)))
+    # don't test with streams since it should be the same result
+    # this better highlights the problem
+    compute(first)
+    compute(second)
     assert global_list == [1]
-    assert lout == [1, 1]
-
-    s2.emit(StreamDoc(args=(myobj)))
-    s2.emit(StreamDoc(args=(myobj)))
-    assert global_list == [1, 1, 1]
-    assert l2out == [1, 1]
