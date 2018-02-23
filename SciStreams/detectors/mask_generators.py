fixed masking

SciStreams/detectors/mask_generators.py
@@ -11,9 +11,15 @@ def generate_mask(**md):
         This dispatches the generation to a mask generator function
             depending on the metadata.
     '''
+    print("Calling mask generator")
     # get name from det key
     detector_key = md.get('detector_key', None)
-    detector_name = detector_key[::-1].split("_", 1)[-1][::-1]
+    if detector_key is not None:
+        detector_name = detector_key[::-1].split("_", 1)[-1][::-1]
+    else:
+        detector_name = None
+        print("Could not find detector name! (Missing detector_key?)")
+        print("metadata : {}".format(md))
     md['detector_name'] = detector_name
 
     # ensure detector_name exists, else give no mask
@@ -25,26 +31,9 @@ def generate_mask(**md):
     else:
         mask = None
 
+    if mask is not None:
+        print("Mask generation succeeded!")
     return dict(mask=mask)
-
-
-def generate_mask_pilatus300(**md):
-    ''' Generate a mask from detector_key and metadata md
-
-        Parameters
-        ----------
-        detector_key : the key for the image for the detector
-        **md : the metadata
-            should contain `motor_bsphi`, `motor_bsx`, `motor_bsy`
-            `detector_SAXS_x0_pix` and `detector_SAXS_y0_pix`
-            which are the degrees of freedom for the beamstop and beam center
-
-        Notes
-        -----
-        The mask generator takes the position pair as y, x as input, and *not*
-        x, y
-    '''
-    raise NotImplementedError("Error, we don't implement this yet")
 
 import h5py
 def load_blemish(fname):
@@ -71,22 +60,22 @@ def load_mask(fname):
 def load_beamcenter(fname):
     '''
         load beam center. just looks for
-            beam_center_x0
-            beam_center_y0
+            beam_center_x
+            beam_center_y
         in hdf5 file.
     '''
     f = h5py.File(fname, "r")
     # make a copy before closing
-    if 'beam_center_x0' not in f:
-        print("Did not find 'beam_center_x0'")
+    if 'beam_center_x' not in f:
+        print("Did not find 'beam_center_x'")
         return None
-    if 'beam_center_y0' not in f:
-        print("Did not find 'beam_center_y0'")
+    if 'beam_center_y' not in f:
+        print("Did not find 'beam_center_y'")
         return None
-    beamx0 = np.array(f['beam_center_x0'])
-    beamy0 = np.array(f['beam_center_y0'])
+    beamx0 = float(f['beam_center_x'].value)
+    beamy0 = float(f['beam_center_y'].value)
     f.close()
-    return dict(beamx0=beamx0, beamy0=beamy0)
+    return [beamy0, beamx0]
 
 def generate_mask_pilatus2M(**md):
     ''' generate mask from startdocument information
@@ -94,15 +83,22 @@ def generate_mask_pilatus2M(**md):
         This will read from local config files for the mask
         filenames if they exist.
     '''
+    #print("loading a mask. md: {}".format(md))
     detector_key = 'pilatus2M_image'
 
     mask_config = masks_config.get(detector_key, {})
 
     # get filenames from config
+    master_dir = mask_config.get('mask_dir', ".")
     blemish_fname = mask_config.get('blemish', {}).get('filename', None)
+    blemish_fname = master_dir + "/" + blemish_fname
     mask_fname = mask_config.get('mask', {}).get('filename', None)
+    mask_fname = master_dir + "/" + mask_fname
     # get the shape from config
-    mask_shape = mask_config['shape']
+    print(mask_config)
+    mask_shape = np.array(mask_config['shape'])
+
+    #print('mask shape : {}'.format(mask_shape))
 
     if blemish_fname is not None:
         blemish = load_blemish(blemish_fname)
@@ -112,25 +108,34 @@ def generate_mask_pilatus2M(**md):
     if mask_fname is not None:
         mask = load_mask(mask_fname)
         beam_center = load_beamcenter(mask_fname)
+        #print("Got beam center from mask: {}".format(beam_center))
+        #print("filename {}".format(mask_fname))
     else:
-        print("No mask found, ignoring")
+        #print("No mask found, ignoring")
         mask = None
         beam_center = None
 
     # now interpolate the beam center from the actual beam center
-    beamcenterx = md.get('detector_saxs_x0_pix', None)
-    beamcentery = md.get('detector_saxs_y0_pix', None)
+    beamcenterx = md.get('beamx0', None)
+    beamcentery = md.get('beamy0', None)
+
+    if isinstance(beamcenterx, dict):
+        beamcenterx = beamcenterx.get('value', None)
+
+    if isinstance(beamcentery, dict):
+        beamcentery = beamcentery.get('value', None)
+
     if beamcenterx is not None and beamcentery is not None:
-        beam_center_experiment = dict(beam_center_x0=beamcenterx,
-                                      beam_center_y0=beamcentery,
-                                      )
+        beam_center_experiment = [beamcentery, beamcenterx]
     else:
         beam_center_experiment = None
         print("No beam center found")
 
     mask_expt = None
     if mask is not None and beam_center_experiment is not None:
-        mask_expt = make_subimage(mask, beam_center, beam_center_experiment)
+        print("got a mask!")
+        mask_expt = make_subimage(mask, beam_center, mask_shape, beam_center_experiment)
+        print("Made sub image")
     else:
         if mask is None:
             print("No mask found")
@@ -138,6 +143,7 @@ def generate_mask_pilatus2M(**md):
             print("No beam center found")
 
     if blemish is not None:
+        print("got a blemish!")
         if mask_expt is None:
             mask_expt = blemish
         else:
@@ -149,6 +155,25 @@ def generate_mask_pilatus2M(**md):
     return mask_expt
 
 
+def generate_mask_pilatus300(**md):
+    ''' Generate a mask from detector_key and metadata md
+
+        Parameters
+        ----------
+        detector_key : the key for the image for the detector
+        **md : the metadata
+            should contain `motor_bsphi`, `motor_bsx`, `motor_bsy`
+            `detector_SAXS_x0_pix` and `detector_SAXS_y0_pix`
+            which are the degrees of freedom for the beamstop and beam center
+
+        Notes
+        -----
+        The mask generator takes the position pair as y, x as input, and *not*
+        x, y
+    '''
+    raise NotImplementedError("Error, we don't implement this yet")
+
+
 # hard coded mask generators built into the library
 mask_generators = dict(pilatus300=generate_mask_pilatus300,
                        pilatus2M=generate_mask_pilatus2M)
@@ -158,7 +183,7 @@ mask_generators = dict(pilatus300=generate_mask_pilatus300,
 from scipy.interpolate import RegularGridInterpolator
 import numpy as np
 
-def make_subimage(master_image, refpoint, shape, new_refpoint):
+def make_subimage(master_image, refpoint, new_shape, new_refpoint):
     ''' Make a subimage from the master image.
 
         Parameters
@@ -169,7 +194,7 @@ def make_subimage(master_image, refpoint, shape, new_refpoint):
         refpoint: number
             The reference point
 
-        shape: 2-tuple
+        new_shape: 2-tuple
             The desired shape of the sub image
 
         new_refpoint: number
@@ -183,19 +208,29 @@ def make_subimage(master_image, refpoint, shape, new_refpoint):
         Internally, all reference points refer as y, x
         Interpolation is used for subpixel shifts
     '''
-    # print("refpoint is {}".format(refpoint))
+    print("refpoint is {}".format(refpoint))
+    print("new refpoint is {}".format(new_refpoint))
+    print("master image shape : {}".format(master_image.shape))
+    print("new shape : {}".format(new_shape))
     x_master = np.arange(master_image.shape[1]) - refpoint[1]
     y_master = np.arange(master_image.shape[0]) - refpoint[0]
 
+    print("initializing interpolator")
     interpolator = RegularGridInterpolator((y_master, x_master), master_image,
                                            bounds_error=False, fill_value=0)
 
+    print("interpolating")
     # make sub image, also make origin the ref point
-    x = np.arange(shape[1]) - new_refpoint[1]
-    y = np.arange(shape[0]) - new_refpoint[0]
+    print('new_shape[1] : {}'.format(new_shape[1]))
+    print('new_refpoint[1] : {}'.format(new_refpoint[1]))
+    x = np.arange(int(new_shape[1])) - new_refpoint[1]
+    print("x dim: {}".format(x))
+    y = np.arange(new_shape[0]) - new_refpoint[0]
     X, Y = np.meshgrid(x, y)
     points = (Y.ravel(), X.ravel())
+    print("points : {}".format(points))
     # it's a linear interpolator, so we just cast to ints (non-border regions
     # should just be 1)
-    subimage = interpolator(points).reshape(shape)
+    subimage = interpolator(points).reshape(new_shape)
+    print("done interpolating")
     return subimage